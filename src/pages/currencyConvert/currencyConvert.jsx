c394007ad09d21aa73b24437d0efb1695c3fed5c
Harsh Mishra <harshmiishra121@gmail.com>
2025-08-01 05:10:48 +0530

    Your update message

--- src/pages/currencyConvert/currencyConvert.jsx
+++ src/pages/currencyConvert/currencyConvert.jsx
@@ -1,22 +1,25 @@
 import styles from "./currencyConvert.module.css";
 import Form from "../../components/form/form";
+import { useSelector } from "react-redux";
 import currencyCode from "../../StaticData/currencyCodeNames";
 import { useState } from "react";
 function CurrencyConvert(){
-    const [means,setMeans]=useState("code")
+    const [means,setMeans]=useState("code");
+    const color=useSelector((state)=>(state.toggle.color))
 
     function chngeMeans(means){
         setMeans(means)
     }
+
     return (
         <div id={styles.container}>
             <div id={styles.Container}>
                 <Form means={means}></Form>
             </div>
             <div style={{marginTop:"5rem"}}>
-                <button onClick={()=>chngeMeans("name")}>Search By Currency name</button>
-            <button onClick={()=>chngeMeans("code")}>Search By currency code</button>
-            <button onClick={()=>chngeMeans("country")}>Search By Country name</button>
+            <button className={styles.convertBtn} style={{backgroundColor:`${color.BG_COLOR }`, color:`${color.FONT_COLOR}`}} onClick={()=>chngeMeans("name")}>Search By Currency name</button>
+            <button className={styles.convertBtn} style={{backgroundColor:`${color.BG_COLOR }`, color:`${color.FONT_COLOR}`}} onClick={()=>chngeMeans("code")}>Search By currency code</button>
+            <button className={styles.convertBtn} style={{backgroundColor:`${color.BG_COLOR }`, color:`${color.FONT_COLOR}`}} onClick={()=>chngeMeans("country")}>Search By Country name</button>
             </div>
             
         </div>
